9bddfcb7bf8c3cde622896330a061867f6c08fa0
zeon77 <zeon77@gmail.com>
2021-06-07 15:55:38 +0200

    Add project files.

---
+++ Operátorok/Expression.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operátorok
+{
+    class Expression
+    {
+        public enum Operators
+        {
+            [Description("+")] addition,
+            [Description("-")] subtraction,
+            [Description("*")] multiplication,
+            [Description("/")] division,
+            [Description("mod")] mod,
+            [Description("div")] div
+        }
+        public int OperandA { get; set; }
+        public int OperandB { get; set; }
+        public Operators Operator { get; set; }
+
+        public Expression(string line)
+        {
+            string[] s = line.Split(' ');
+            OperandA = int.Parse(s[0]);
+            OperandB = int.Parse(s[2]);
+            Operator = (Operators) Enum.Parse(typeof(Operators), s[1], true);
+        }
+    }
+}
---
+++ Operátorok/Operátorok.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Operátorok
+{
+    class Operátorok
+    {
+        static void Main(string[] args)
+        {
+            //1.
+            List<Expression> expressions = new List<Expression>();
+            foreach (var line in File.ReadAllLines("kifejezesek.txt").Skip(1))
+                expressions.Add(new Expression(line));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
